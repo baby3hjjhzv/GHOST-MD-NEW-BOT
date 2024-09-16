746a5f92db4049e3fc58d2fe59614e96037e5997
baby3hjjhzv <nawolak68@gmail.com>
2024-09-16 11:08:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB:process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "03MwGYLY#PN7F5veG0hJL8lL1Sn9Y9m6sMLfVxQUBGjWq4kFR7Vs",
+MONGODB:process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 };
